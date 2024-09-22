901a23cb5cdbaa7a52e8ec0d19a2e230faeff67a
Saetoff <Saetoff.Ainur@gmail.com>
2024-09-23 00:41:48 +0300

    training

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+my_string = input()
+print(len(my_string)) # количество символов
+print(my_string .upper()) # верхний регистр
+print(my_string .lower())  # нижний регистр
+print(my_string .replace(" ","")) # без пробелов
+print(my_string[0])
+print(my_string[-1])
---
+++ peremennye.py
@@ -0,0 +1,5 @@
+completed_tasks = 12
+number_of_hours = 1.5
+course_name = "Python"
+time_for_the_task = number_of_hours / completed_tasks
+print("Курс:",course_name,", ""всего задач:" ,completed_tasks, ", " "затрачено часов:", number_of_hours, ", ""среднее время выполнения", time_for_the_task, "часа." )
