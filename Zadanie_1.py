e937ff654fe214fdc58e02de299de087d0c20387
Saetoff <Saetoff.Ainur@gmail.com>
2024-09-30 04:19:10 +0300

    training

--- Zadanie_1.py
+++ Zadanie_1.py
@@ -1,5 +1,3 @@
-from turtledemo.chaos import jumpto
-
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 students_list = sorted(list(students))
