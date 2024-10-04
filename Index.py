a7745f2eae85567f16b59ceb92e16d04638b43d9
Saetoff <Saetoff.Ainur@gmail.com>
2024-10-04 11:31:27 +0300

    training

--- Index.py
+++ Index.py
@@ -1,5 +1,5 @@
 example="Топинамбур"
-print(example[0])
+print(example[0:])
 print(example[-1])
 print(example[5:])
 print(example[::-1])
---
+++ module_2_4.py
@@ -0,0 +1,8 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = []
+not_primes = []
+for i in range(len(numbers)):
+    if i > 1:
+        for i in numbers:
+            if i % i == 0:
+                print(i)
