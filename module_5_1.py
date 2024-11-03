371036603d7a8391a3803adea288adc2178b15ed
Saetoff <Saetoff.Ainur@gmail.com>
2024-11-03 19:04:15 +0300

    training

--- module_5_1.py
+++ module_5_1.py
@@ -1,4 +1,10 @@
 class House:
+    houses_history = []
+
+    def __new__(cls, *args, **kwargs):
+        cls.houses_history.append(args[0])
+        
+
     def __init__(self, name, number_of_floors):
         self.name = name
         self.number_of_floors = number_of_floors
@@ -52,7 +58,8 @@ class House:
     def __iadd__(self, value):
         return self.__add__(value)
 
-
+    def __del__(self):
+        print(f'{self.name} снесен, но он остается в истории')
 
 
 
