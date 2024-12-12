15b585384a3c5ff213c90edb481153c31a458bba
Saetoff <Saetoff.Ainur@gmail.com>
2024-12-12 19:36:32 +0300

    training

--- mine.py
+++ mine.py
@@ -1,1 +1,12 @@
-import os # импорт модуля операционной системы
+def add_everything_up(a, b):
+    try:
+        result = a + b
+    except TypeError:
+        return f' {a}, {b}'  # Возвращаем сообщение об ошибке
+    else:
+        return f'{result: .3f}'  # Возвращаем результат сложения
+
+# Вызываем функцию и выводим результат
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
